dbcbaa8bc378c73e8a72d2452c98ab9dd08baacf
Developer <44878271+advantage-development@users.noreply.github.com>
2018-12-06 16:07:57 -0600

    Update bitrpc.py

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -24,7 +24,7 @@ if cmd == "backupwallet":
 
 elif cmd == "getaccount":
 	try:
-		addr = raw_input("Enter a Bitcoin address: ")
+		addr = raw_input("Enter an Advantage address: ")
 		print access.getaccount(addr)
 	except:
 		print "\n---An error occurred---\n"
